878524a50068fb5cb232961d482a6d4e14d07357
sdyer777 <sdyer777@yahoo.com>
2017-06-29 14:03:36 -0400

    Fully working copy. Barchart implemented in D3, from data composed in the view.

--- codesample/static/codesample/barchart.js
+++ codesample/static/codesample/barchart.js
@@ -1,27 +1,66 @@
 function drawChart() {
-    alert("blabbo-gregory -- this is in barchar.js static file");
+	var margin = {top: 20, right: 20, bottom: 70, left: 40},
+		width = 600 - margin.left - margin.right,
+		height = 300 - margin.top - margin.bottom;
+
+	// Parse the date / time
+	var	parseDate = d3.time.format("%Y-%m-%d").parse;
+
+	var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+	var y = d3.scale.linear().range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom")
+		.tickFormat(d3.time.format("%Y-%m-%d"));
+
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left")
+		.ticks(10);
+
+	var svg = d3.select("body").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+		.attr("transform", 
+			  "translate(" + margin.left + "," + margin.top + ")");
+
+	data.forEach(function(d) {
+		d.visit_date = parseDate(d.visit_date);
+		d.avg_wait_time = +d.avg_wait_time;
+	});
+	
+	x.domain(data.map(function(d) { return d.visit_date; }));
+	y.domain([0, d3.max(data, function(d) { return d.avg_wait_time; })]);
+
+	svg.append("g")
+	  .attr("class", "x axis")
+	  .attr("transform", "translate(0," + height + ")")
+	  .call(xAxis)
+	.selectAll("text")
+	  .style("text-anchor", "end")
+	  .attr("dx", "-.8em")
+	  .attr("dy", "-.55em")
+	  .attr("transform", "rotate(-90)" );
+
+	svg.append("g")
+	  .attr("class", "y axis")
+	  .call(yAxis)
+	.append("text")
+	  .attr("transform", "rotate(-90)")
+	  .attr("y", -30)
+	  .style("text-anchor", "end")
+	  .text("Average Wait (min)");
+
+	svg.selectAll("bar")
+	  .data(data)
+	.enter().append("rect")
+	  .style("fill", "steelblue")
+	  .attr("x", function(d) { return x(d.visit_date); })
+	  .attr("width", x.rangeBand())
+	  .attr("y", function(d) { return y(d.avg_wait_time); })
+	  .attr("height", function(d) { return height - y(d.avg_wait_time); });
     }
 
-
-var dataArray = [23, 13, 21, 14, 37, 15, 18, 34, 30];
-
-var svg = d3.select("body").append("svg")
-          .attr("height","100%")
-          .attr("width","100%");
-          
-svg.selectAll("rect")
-    .data(dataArray)
-    .enter().append("rect")
-    	  .attr("class", "bar")
-          .attr("height",function(d, i) {return (d * 10)})
-          .attr("width","40")
-          .attr("x",function(d, i) {return (i * 60) + 25})
-          .attr("y",function(d, i) {return 400 - (d * 10)});
-          
-svg.selectAll("text")
-    .data(dataArray)
-    .enter().append("text")
-    .text(function(d) {return d;})
-		  .attr("class", "text")
-          .attr("x", function(d, i) {return (i * 60) + 36})
-          .attr("y", function(d, i) {return 415 - (d * 10)});
--- codesample/views.py
+++ codesample/views.py
@@ -4,14 +4,43 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.template import loader
+from django.db.models import Count, Avg
 
 from .models import WaitTime
 
 csvfilename = ''
 
 def index(request):
-    wait_time_list = WaitTime.objects.order_by('visit_date')[:100]
+#    wait_time_list = WaitTime.objects.order_by('visit_date')[:1000]
+    wait_time_list = WaitTime.objects.extra({'visit_date':"date(visit_date)"}).values('visit_date').annotate(avg_wait_time=Avg('wait_time')).annotate(visit_count=Count('visit_date', distinct=True)).order_by('visit_date')[:1000]
     template = loader.get_template('codesample/index.html')
+
+ #   zz = WaitTime.objects.annotate(bubba=321).filter(wait_time=111).count()
+#    zz = WaitTime.objects.extra({'visit_date':"date(visit_date)"}).values('visit_date', 'patient_type').annotate(visit_count=Count('id')).order_by('visit_date')
+#    zz = WaitTime.objects.extra({'visit_date':"date(visit_date)"}).values('visit_date').annotate(avgWaitTime=Avg('wait_time')).order_by('visit_date')
+#    zz = WaitTime.objects.extra({'visit_date':"date(visit_date)"}).values('visit_date').annotate(avgWaitTime=Avg('wait_time')).annotate(visitcount=Count('visit_date', distinct=True)).order_by('visit_date')
+#    print (zz)
+
+#    print (wait_time_list)
+#    print ('---------------------------------')
+ #   print(wait_time_list[0]['visit_date'])
+
+#    rrlist = WaitTime.objects.order_by('visit_date')[:1000]
+#    print (rrlist)
+#    for wt in rrlist:
+#        print (wt.wait_time)
+#    qq = max(wt.wait_time for wt in rrlist)
+#    print (qq)
+
+    
+     
+#    rr = max(wt.avg_wait_time for wt in wait_time_list)
+#    print (rr)
+    
+#    yy = max(wt.avg_wait_time for wt in wait_time_list)
+#    print (yy)
+    
+
     context = {
         'wait_time_list': wait_time_list,
         'csvfilename': csvfilename
@@ -19,27 +48,24 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def uploadfile(request):
-    # upload CSV file, validate it, replace data in DB
+    """upload CSV file, validate it, replace data in DB
+    """
     if request.method == 'POST' and request.FILES['csvfile']:
+        # open file
         csvfile = request.FILES['csvfile']
         fs = FileSystemStorage()
         csvfilename = fs.save(csvfile.name, csvfile)
         uploaded_file_url = fs.url(csvfilename)
-        # open file
         # validate file
-        # erase data
+        # << TO DO >>
+        # erase existing data
         WaitTime.objects.all().delete()
         # read CSV data into database
         f = open(csvfilename, 'r')  
         for line in f:
             line =  line.split(',')
             print (line)
-            tmp = WaitTime(visit_date=line[0], patient_type=line[1], wait_time=line[2])
- #           tmp = WaitTime.objects.create()
- #           tmp.visit_date = line[0]
- #           tmp.visit_date = 'June 22, 2017, 10 a.m.'
- #           tmp.patient_type = line[1]
- #           tmp.wait_time = line[2]
+            tmp = WaitTime(visit_date=line[0], patient_type=line[1].strip(), wait_time=line[2])
             tmp.save()
         f.close()
         # redirect to index, which shows the new data
